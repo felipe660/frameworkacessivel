b5b90b88bce9e52962f29739cc7fa85b426175ee
felipe660 <felipe.s.milani@gmail.com>
2025-05-27 02:30:46 -0300

    ajustes gerais

---
+++ projects/core/src/lib/modal/modal.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'lib-modal',
+  standalone: true, // 👈 adicione esta linha
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+  @Input() titulo: string = '';
+  @Input() descricao: string = '';
+  @Input() aberto: boolean = false;
+  @Output() fechado = new EventEmitter<void>();
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    if (this.aberto) {
+      setTimeout(() => {
+        const modal = this.el.nativeElement.querySelector('.modal');
+        modal?.focus();
+      }, 0);
+    }
+  }
+
+  fecharModal() {
+    this.fechado.emit();
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEsc(event: KeyboardEvent) {
+    if (this.aberto) {
+      event.preventDefault();
+      this.fecharModal();
+    }
+  }
+}
--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AccessibleInputComponent } from '../../../core/src/lib/accessible-input/accessible-input.component';
 import { ButtonComponent } from "../../../core/src/lib/button/button.component";
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../../../core/src/lib/modal/modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonComponent, AccessibleInputComponent,CommonModule],
+  imports: [RouterOutlet, ButtonComponent, AccessibleInputComponent,CommonModule, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,8 +17,20 @@ enviar() {
   throw new Error('Method not implemented.');
 }
   title = 'demo';
+  modalOpen = false;
   usuario = {nome: ''};
-   handleButtonClick() {
+
+  handleButtonClick() {
     alert('Botão Acessível clicado!');
+  }
+
+  openModal() {
+    console.log('Modal aberto!');
+    console.log(this.modalOpen);
+    this.modalOpen  = true;
+  }
+
+  closeModal() {
+    this.modalOpen = false;
   }
 }
