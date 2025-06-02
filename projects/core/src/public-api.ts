fe3b1007df5b3ac4b34b67318c9c3f84ede2753b
felipe660 <felipe.s.milani@gmail.com>
2025-06-02 01:53:36 -0300

    ajustes gerais

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -4,3 +4,12 @@
 
 export * from './lib/core.service';
 export * from './lib/core.component';
+export * from './lib/alert-list/alert-list.component';
+export * from './lib/button/button.component';
+export * from './lib/collapse/collapse.component';
+export * from './lib/dropdown/dropdown.component';
+export * from './lib/input/accessible-input.component';
+export * from './lib/modal/modal.component';
+export * from './lib/responsive-image/responsive-image.component';
+export * from './lib/select/select.component';
+export * from './lib/table/table.component';
