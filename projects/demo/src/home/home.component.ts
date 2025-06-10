28892703692b26249bd561a2519571a2033c2042
felipe660 <felipe.s.milani@gmail.com>
2025-06-10 15:46:12 -0300

    ajuste de rotas

---
+++ projects/demo/src/home/home.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { AccessibleInputComponent } from '../../../core/src/lib/input/accessible-input.component';
+import { ButtonComponent } from "../../../core/src/lib/button/button.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ModalComponent } from '../../../core/src/lib/modal/modal.component';
+import { TableComponent } from "../../../core/src/lib/table/table.component";
+import { SelectComponent } from "../../../core/src/lib/select/select.component";
+import { AlertListComponent } from "../../../core/src/lib/alert-list/alert-list.component";
+import { CollapseComponent } from "../../../core/src/lib/collapse/collapse.component";
+import { DropdownComponent } from "../../../core/src/lib/dropdown/dropdown.component";
+import { ResponsiveImageComponent } from '../../../core/src/lib/responsive-image/responsive-image.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [ButtonComponent, ResponsiveImageComponent, AccessibleInputComponent, CommonModule, ModalComponent, TableComponent, SelectComponent, AlertListComponent, FormsModule, CollapseComponent, DropdownComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  
+  title = 'FrameWork';
+  selectStatus = '';
+  newMsg = '';
+  modalOpen = false;
+  user = {name: ''};
+  headers = ['Nome', 'Email', 'Status'];
+  tableData = [
+    ['João Silva', 'joao@email.com', 'Ativo'],
+    ['Maria Souza', 'maria@email.com', 'Inativo'],
+    ['Carlos Lima', 'carlos@email.com', 'Ativo']
+  ];
+  statusOptions = [
+  { label: 'Ativo', value: 'Ativo' },
+  { label: 'Inativo', value: 'Inativo' },
+  { label: 'Pendente', value: 'Pendente' }
+  ];
+  alerts = [
+  { id: 1, text: 'Bem-vindo!', ativo: true },
+  ];
+
+  menuOptions = ['Perfil', 'Configurações', 'Sair'];
+
+  selectOption(opcao: string) {
+    alert(`Opção selecionada: ${opcao}`);
+  }
+
+  send() {
+    throw new Error('Method not implemented.');
+  }
+
+  addAlert() {
+    if (this.newMsg.trim()) {
+      const novoId = this.alerts.length > 0 ? Math.max(...this.alerts.map(m => m.id)) + 1 : 1;
+      this.alerts.push({
+        id: novoId,
+        text: this.newMsg.trim(),
+        ativo: true
+      });
+      this.newMsg = ''; // limpa o campo
+    }
+  }
+
+  removeAlert(id: number) {
+    const alerta = this.alerts.find(msg => msg.id === id);
+    if (alerta) {
+      alerta.ativo = false;
+    }
+  }
+
+  handleButtonClick() {
+    alert('Botão Acessível clicado!');
+  }
+
+  openModal() {
+    this.modalOpen = true;
+  }
+
+  closeModal() {
+    this.modalOpen = false;
+  }
+}
---
+++ projects/demo/src/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input } from '@angular/core';
+import { AccessibleInputComponent } from '../../../core/src/lib/input/accessible-input.component';
+import { ButtonComponent } from "../../../core/src/lib/button/button.component";
+import { TableComponent } from "../../../core/src/lib/table/table.component";
+import { SelectComponent } from "../../../core/src/lib/select/select.component";
+import { AlertListComponent } from "../../../core/src/lib/alert-list/alert-list.component";
+
+@Component({
+  selector: 'app-login',
+  imports: [ButtonComponent, AccessibleInputComponent, TableComponent, SelectComponent, AlertListComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  @Input() label: string = '';
+  @Input() ariaLabel: string = ''; 
+  @Input() className: string = ''; 
+  headersData = ['Nome', 'Email', 'Phone', 'Status'];
+  title = 'teste-framework';
+  selectStatus = 'ativo';
+  menuOptions = ['Perfil', 'Configurações', 'Sair'];
+  user: any = { name: '', email: '', phone: '' };
+  statusOptions = [
+  { label: 'Ativo', value: 'ativo' },
+  { label: 'Inativo', value: 'inativo' },
+  { label: 'Pendente', value: 'pendente' }
+  ];
+
+  alerts: { id: number; text: string; ativo: boolean }[] = [];
+
+  tableData: string[][] = [];
+
+  handleButtonClick() {
+    console.log(this.user)
+    if(this.user.name != '' || this.user.email != '' || this.user.phone != ''){
+    const novoItem: string[] = [this.user.name, this.user.email, this.user.phone, this.selectStatus];
+    const novoAlerta = {id:this.alerts.length + 1, text:'Novo usuario adicionado', ativo: true}
+    this.alerts.push(novoAlerta);
+    this.tableData.push(novoItem);
+    console.log(this.tableData);
+    }
+  }
+
+  removeAlert(id: number) {
+    const alerta = this.alerts.find(msg => msg.id === id);
+    if (alerta) {
+      alerta.ativo = false;
+    }
+  }
+
+
+}
